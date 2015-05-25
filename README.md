1ad9569914863fd61c143cbb11176a8ec69310e6
rainbowism <sanny@rainbowism.com>
2015-05-25 00:26:16 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # gin-pongo2
 pongo2 middleware for Gin framework.
+
+Example:
+
+```Go
+package main
+
+import (
+    "os"
+
+    "github.com/gin-gonic/gin"
+    "github.com/rainbowism/gin-pongo2"
+)
+
+func main() {
+    switch os.Getenv("MODE") {
+    case "RELEASE":
+        gin.SetMode(gin.ReleaseMode)
+
+    case "DEBUG":
+        gin.SetMode(gin.DebugMode)
+
+    case "TEST":
+        gin.SetMode(gin.TestMode)
+
+    default:
+        gin.SetMode(gin.ReleaseMode)
+    }
+
+    router := gin.New()
+    router.Use(gin.Recovery())
+
+    if gin.IsDebugging() {
+        router.HTMLRender = render.NewDebug("resources")
+    } else {
+        router.HTMLRender = render.NewProduction("resources")
+    }
+
+    router.Static("/static", "resources/static")
+    router.GET("/", IndexGet)
+
+    router.Run(":3000")
+}
+```
---
+++ render.go
@@ -0,0 +1,73 @@
+package render
+
+import (
+	"log"
+	"net/http"
+	"path"
+
+	"github.com/flosch/pongo2"
+	"github.com/gin-gonic/gin/render"
+)
+
+type (
+	PongoProduction struct {
+		Templates map[string]*pongo2.Template
+		Path      string
+	}
+
+	PongoDebug struct {
+		Path string
+	}
+
+	Pongo struct {
+		Template *pongo2.Template
+		Name     string
+		Data     interface{}
+	}
+)
+
+func NewProduction(path string) *PongoProduction {
+	return &PongoProduction{map[string]*pongo2.Template{}, path}
+}
+
+func NewDebug(path string) *PongoDebug {
+	return &PongoDebug{path}
+}
+
+func (p PongoProduction) Instance(name string, data interface{}) render.Render {
+	var t *pongo2.Template
+	if tmpl, ok := p.Templates[name]; ok {
+		t = tmpl
+	} else {
+		tmpl, err := pongo2.FromFile(path.Join(p.Path, name))
+		if err != nil {
+			panic(err)
+		}
+		p.Templates[name] = tmpl
+		t = tmpl
+	}
+	return Pongo{
+		Template: t,
+		Name:     name,
+		Data:     data,
+	}
+}
+
+func (p PongoDebug) Instance(name string, data interface{}) render.Render {
+	log.Print("recompiling ", name)
+	t, err := pongo2.FromFile(path.Join(p.Path, name))
+	if err != nil {
+		panic(err)
+	}
+	return Pongo{
+		Template: t,
+		Name:     name,
+		Data:     data,
+	}
+}
+
+func (p Pongo) Write(w http.ResponseWriter) error {
+	ctx := p.Data.(pongo2.Context)
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	return p.Template.ExecuteWriter(ctx, w)
+}
